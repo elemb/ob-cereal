c1a8666fbfb88e46696afd4de54eaee18eaec12d
LM Butler <68486806+elemb@users.noreply.github.com>
2026-01-28 15:45:25 -0700

    Update UART parameters and execution function

--- ob-uart.el
+++ ob-uart.el
@@ -47,86 +47,103 @@
   :group 'ob-uart
   :type 'boolean)
 
+(require 'cl-lib)
+(require 'ob)
+
 (defvar org-babel-default-header-args:uart
-  `((:ienc . "raw")
-    (:oenc . "raw")
-    (:port . "/dev/ttyUSB0")
-    (:speed . 9600)
+  '((:port . "/dev/ttyUSB0")
+    (:speed . 115200)
     (:bytesize . 8)
-    (:parity . nil)
+    (:parity . "none")
     (:stopbits . 1)
-    (:flowcontrol . nil)
-    (:timeout . 1)
-    (:lineend . "\n"))
-  "Default arguments for evaluating a UART block.")
+    (:flowcontrol . "none")
+    (:timeout . 2)
+    (:lineend . "\r")
+    (:wait-for . "ok")
+    (:ienc . "raw")
+    (:oenc . "raw")))
 
-;;;###aut
+(defun ob-uart--normalize-params (params)
+  (cl-labels
+      ((sym (v)
+            (pcase (and v (downcase v))
+              ((or "none" "nil") nil)
+              ("even" 'even)
+              ("odd"  'odd)
+              ("hardware" 'hardware)
+              ("software" 'software)
+              (_ nil))))
+    (list
+     :port (cdr (assoc :port params))
+     :speed (string-to-number (cdr (assoc :speed params)))
+     :bytesize (string-to-number (cdr (assoc :bytesize params)))
+     :parity (sym (cdr (assoc :parity params)))
+     :stopbits (string-to-number (cdr (assoc :stopbits params)))
+     :flowcontrol (sym (cdr (assoc :flowcontrol params)))
+     :timeout (string-to-number (cdr (assoc :timeout params)))
+     :lineend (cdr (assoc :lineend params))
+     :wait-for (cdr (assoc :wait-for params))
+     :ienc (cdr (assoc :ienc params))
+     :oenc (cdr (assoc :oenc params)))))
+
+(defun ob-uart--open (p)
+  (make-serial-process
+   :name "ob-uart"
+   :buffer (generate-new-buffer " *ob-uart*")
+   :port (plist-get p :port)
+   :speed (plist-get p :speed)
+   :bytesize (plist-get p :bytesize)
+   :parity (plist-get p :parity)
+   :stopbits (plist-get p :stopbits)
+   :flowcontrol (plist-get p :flowcontrol)))
+
+(defun ob-uart--encode (s enc)
+  (pcase enc
+    ("raw" s)
+    ("hex"
+     (apply #'unibyte-string
+            (mapcar (lambda (x) (string-to-number x 16))
+                    (split-string s "[ \t\n]+" t))))
+    (_ s)))
+
+(defun ob-uart--read (proc timeout wait-for)
+  (let ((buf (process-buffer proc))
+        (start (float-time))
+        done)
+    (with-current-buffer buf (erase-buffer))
+    (while (and (process-live-p proc)
+                (< (- (float-time) start) timeout)
+                (not done))
+      (accept-process-output proc 0.1)
+      (when wait-for
+        (with-current-buffer buf
+          (when (string-match-p wait-for (buffer-string))
+            (setq done t)))))
+    (with-current-buffer buf
+      (buffer-string))))
+
+(defun ob-uart--decode (s enc)
+  (pcase enc
+    ("raw" s)
+    ("hex"
+     (mapconcat (lambda (b) (format "%02x" b))
+                (string-to-list s) " "))
+    (_ s)))
+
+;;;###autoload
 (defun org-babel-execute:uart (body params)
-  "Execute a block of Lemonbeat code with org-babel.
-This function is called by `org-babel-execute-src-block'
-Argument BODY content to send.
-Argument PARAMS UART communication parameters."
-  (message "executing UART source code block")
-(let* ((ienc (cdr (assoc :ienc params)))
-       (oenc (cdr (assoc :oenc params)))
-       (port (cdr (assoc :port params)))
-       (speed (string-to-number (cdr (assoc :speed params))))
-       (bytesize (string-to-number (cdr (assoc :bytesize params))))
-       (stopbits (string-to-number (cdr (assoc :stopbits params))))
-       (timeout (string-to-number (cdr (assoc :timeout params))))
-       (parity (cdr (assoc :parity params)))
-       (flowcontrol (cdr (assoc :flowcontrol params)))
-       (lineend (cdr (assoc :lineend params)))
-       (process (format "ob-uart-%s" port))
-       (process-buffer (format "*ob-uart-%s*" port)))
-
-
-(make-serial-process
- :name process
- :buffer process-buffer
- :port port
- :speed speed
- :bytesize bytesize
- :parity parity
- :stopbits stopbits
- :flowcontrol flowcontrol
- :filter #'ob-uart-listen-filter)
-
-    (when (string= "hex" ienc)
-	(setq body (mapconcat (lambda (x) (byte-to-string (string-to-number x 16))) (split-string body) "")))
-
-    (process-send-string process (concat body lineend))
-    (with-current-buffer process-buffer
-      (erase-buffer))
-
-    (sleep-for timeout)
-
-    (let ((result))
-      (with-current-buffer process-buffer
-	(setq result (buffer-string)))
-      (delete-process process)
-      (kill-buffer process-buffer)
-
-      (when (string= "hex" oenc)
-	  (setq result (mapconcat (lambda (x) (format "%02x" x)) (string-to-list result) " ")))
-
-      (when (string= "HEX" oenc)
-	  (setq result (mapconcat (lambda (x) (format "%02X" x)) (string-to-list result) " ")))
-
-      (with-temp-buffer
-	(insert result)
-	(when (not (string= "raw" oenc))
-	    (fill-region (point-min) (point-max)))
-	(buffer-string)))))
-
-(defun ob-uart-listen-filter (proc string)
-  "Filter to process response.
-Argument PROC process.
-Argument STRING response string."
-  (when ob-uart-debug
-      (message (format "ob-uart got %d bytes" (string-bytes string))))
-  (with-current-buffer (format "*%s*" (process-name proc))
-    (insert string)))
-
-(provide 'ob-uart)
-;;; ob-uart.el ends here
+  (let* ((p (ob-uart--normalize-params params))
+         (proc (ob-uart--open p)))
+    (unwind-protect
+        (progn
+          (process-send-string
+           proc
+           (concat (ob-uart--encode body (plist-get p :ienc))
+                   (plist-get p :lineend)))
+          (ob-uart--decode
+           (ob-uart--read proc
+                          (plist-get p :timeout)
+                          (plist-get p :wait-for))
+           (plist-get p :oenc)))
+      (when (process-live-p proc) (delete-process proc))
+      (kill-buffer (process-buffer proc)))))
